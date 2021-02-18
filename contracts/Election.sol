6a4317219340ab2c50445959fc0a3d47098a082a
Rajat Arora <rajatarora2812@gmail.com>
2021-02-19 00:32:23 +0530

    add events to render vote count

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -5,16 +5,23 @@ contract Election {
     // Model a Candidate
     struct Candidate {
         string name;
-        uint256 id;
-        uint256 voteCount;
+        uint id;
+        uint voteCount;
     }
+
+    event votedEvent(
+        uint indexed _candidateId
+    );
+
+    // stores the voter address to avoid mutiple votes
+    mapping(address => bool) public voters;
 
     // Store Candidates
     // Fetch Candidate
-    mapping(uint256 => Candidate) public candidates;
+    mapping(uint => Candidate) public candidates;
 
     // Read Candidates Count
-    uint256 public candidateCount;
+    uint public candidateCount;
 
     constructor() {
         addCandidate("Rajat");
@@ -26,5 +33,21 @@ contract Election {
     function addCandidate(string memory _name) private {
         candidateCount++;
         candidates[candidateCount] = Candidate(_name, candidateCount, 0);
+    }
+
+    function vote(uint _candidateId) public {
+        // verify the voters has not voted before
+        require(!voters[msg.sender]);
+
+        // candiadte Id should be valid
+
+        require(_candidateId > 0 && _candidateId <= candidateCount);
+
+        // Record that voter has voted
+        voters[msg.sender] = true;
+        // update candidate vote count
+        candidates[_candidateId].voteCount++;
+
+       emit votedEvent(_candidateId);
     }
 }
--- test/election.js
+++ test/election.js
@@ -3,6 +3,7 @@ var Election = artifacts.require("./Election.sol");
 contract("Election", function (accounts) {
 
     var electionInstance;
+    var candidateId;
 
     it("initialize with four candidates", function () {
         return Election.deployed().then(function (instance) {
@@ -27,4 +28,50 @@ contract("Election", function (accounts) {
             assert.equal(secondCandidate[2], 0, "Default Vote Count");
         });
     });
+
+    it("increase vote count of candidate when someone votes and store ", function () {
+        return Election.deployed().then(function (instance) {
+            electionInstance = instance;
+            candidateId = 1;
+            return electionInstance.vote(candidateId, { from: accounts[0] });
+        }).then(function (receipt) {
+            return electionInstance.voters(accounts[0]);
+        }).then(function (voted) {
+            assert(voted, "the voter marked as voted")
+            return electionInstance.candidates(candidateId);
+        }).then(function (candidate) {
+            var voteCount = candidate[2];
+            assert(voteCount, 1, "Vote is incrmented");
+        });
+    });
+
+    it("throw exception in case of multiple voting attemps by same voter", function () {
+        return Election.deployed().then(function (instance) {
+            electionInstance = instance;
+            return electionInstance.vote(3, { from: accounts[0] });
+        }).then(function (receipt) {
+            return electionInstance.vote(3, { from: accounts[0] });
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
+        });
+    });
+
+    it("throw exception in case of invalid candidate", function () {
+        return Election.deployed().then(function (instance) {
+            electionInstance = instance;
+            return electionInstance.vote(99, { from: accounts[0] });
+        }).then(assert.fail).catch(function (error) {
+            assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
+            return electionInstance.candidates(1)
+        }).then(function (candidate1) {
+            var voteCount = candidate1[2];
+            assert.equal(voteCount, 1, "candidate didnot received ant new votes");
+            return electionInstance.candidates(2);
+        }).then(function (candidate2) {
+            var voteCount = candidate2[2];
+            assert.equal(voteCount, 0, "vote count is zero");
+        });
+    });
+
+
 });
